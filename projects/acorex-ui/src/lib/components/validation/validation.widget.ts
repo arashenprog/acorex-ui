9b1739f53c7a92a9089b087d99f9b74e58b2a94a
a.safari <a.safari@SAFARIA>
2019-04-29 17:35:50 +0430

    validation style

--- projects/acorex-ui/src/lib/components/validation/validation.widget.ts
+++ projects/acorex-ui/src/lib/components/validation/validation.widget.ts
@@ -7,7 +7,7 @@ import { IValidationResult } from "./validation.classs";
 })
 export class AXValidation {
   @Input()
-  type: "required" | "email" | "regex" = "required";
+  type: "required" | "email" | "regex" | "phone" = "required";
   @Input()
   message: string;
   @Input()
@@ -20,9 +20,14 @@ export class AXValidation {
           resolve({ message: this.message, result: value != null });
           break;
         case "email":
-          const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          resolve({ message: this.message, result: reg.test(value) });
+          const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          resolve({ message: this.message, result: regEmail.test(value) });
           break;
+          case "phone":
+          const regPhone = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+          resolve({ message:this.message, result: regPhone.test(value) });
+
+          break
         case "regex":
           resolve({
             message: this.message,
