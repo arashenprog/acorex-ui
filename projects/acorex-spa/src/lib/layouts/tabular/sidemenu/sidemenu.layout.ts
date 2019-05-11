3700dfc55e48e53b753f274acb432cd23df42ffb
arashenprog <arash.oshnoudi@gmail.com>
2019-05-11 16:32:32 +0430

    side menu search

--- projects/acorex-spa/src/lib/layouts/tabular/sidemenu/sidemenu.layout.ts
+++ projects/acorex-spa/src/lib/layouts/tabular/sidemenu/sidemenu.layout.ts
@@ -1,72 +1,87 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
 import { MenuItem } from "acorex-ui"
+import { distinctUntilChanged, debounceTime } from 'rxjs/operators';
 import { AXNavMenuService } from '../../shared/services/nav-menu.service';
+import { Observable } from 'rxjs';
+import { InternalFormsSharedModule } from '@angular/forms/src/directives';
 
 
 @Component({
   selector: "ax-side-menu",
-  templateUrl: "./sidemenu.layout.html"
+  templateUrl: "./sidemenu.layout.html",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AXSideMenuComponent {
 
 
-
-  constructor(private navMenuService: AXNavMenuService) {
-
+  constructor(private navMenuService: AXNavMenuService, private changeDetectorRef: ChangeDetectorRef) {
   }
 
   searchText: string;
+  searchChangeObserver: any;
+
   current: MenuItem = null;
   favoriteCheck: boolean = false;
-  private items: MenuItem[];
+  private items: MenuItem[] = [];
   private allItems: MenuItem[] = [];
-  currentItems: MenuItem[] = []
+  private favoriteItems: MenuItem[] = []
+  displayItems: any[] = [];
 
 
-  // get currentItems(): Promise<MenuItem[]> {
-  //   return new Promise((resolve, reject) => {
-  //     if (this.searchText) {
-  //       return this.search(this.searchText);
-  //     } else {
-  //       if (this.favoriteCheck) resolve(this.favoriteItems);
-  //       else return resolve(this.items.filter(c => c.visible));
-  //     }
-  //   });
-  // }
-
-  favoriteItems: MenuItem[] = []
-
-  ngOnInit() {
-    this.navMenuService.getItems().then(c => {
-      debugger;
-      this.allItems = c;
-      this.refresh();
-    })
-    this.navMenuService.getFavorites().then(c => {
-      this.allItems = c;
-    })
-  }
 
   private refresh() {
-    this.items = this.allItems.filter(c => c.parentId == null).slice();
     if (this.searchText) {
-      this.search(this.searchText).then(d => {
-        this.currentItems = d;
+      this.navMenuService.serach(this.searchText).then(c => {
+        this.displayItems = c.map(c => this.mapToDisplay(c));
+        this.changeDetectorRef.markForCheck();
       });
     } else {
       if (this.favoriteCheck)
-        this.currentItems = this.favoriteItems.slice();
+        this.displayItems = this.favoriteItems.map(c => this.mapToDisplay(c));
       else
-        this.currentItems = this.items.filter(c => c.visible).slice();
+        this.displayItems = this.items.slice().map(c => this.mapToDisplay(c));
+      //
+      this.changeDetectorRef.markForCheck();
     }
   }
 
-  onItemClick(item: MenuItem) {
-    this.navMenuService.clickItem(item)
+
+  private mapToDisplay(item: MenuItem) {
+    let i: any = Object.assign({}, item);
+    i.isRoot = this.allItems.some(c => c.parentId == item.id);
+    i.isFav = this.allItems.some(c => c.id == item.id);
+    return i;
   }
 
 
+  ngOnInit() {
+    this.navMenuService.getItems().then(c => {
+      this.allItems = c;
+      this.items = this.allItems.filter(c => !c.parentId).slice();
+      this.refresh();
+    })
+    this.navMenuService.getFavorites().then(c => {
+      this.favoriteItems = c;
+    })
+  }
+
+
+  onItemClick(item: any) {
+    if (item.isRoot) {
+      this.current = item;
+      this.items = this.allItems
+        .filter(c => c.parentId == item.id)
+        .slice();
+      this.refresh();
+    } else {
+      this.navMenuService.clickItem(item).then(c=>{
+          //
+      });
+    }
+  }
+
   onBackClick() {
+    debugger;
     if (this.current && this.current.parentId) {
       this.items = this.allItems
         .filter(c => c.parentId == this.current.parentId)
@@ -76,22 +91,13 @@ export class AXSideMenuComponent {
       );
     } else {
       this.current = null;
+      this.items = this.allItems.filter(c => !c.parentId).slice();
       this.refresh();
     }
   }
 
-  private search(text: string): Promise<MenuItem[]> {
-    return new Promise((resolve, reject) => {
-      this.navMenuService.serach(text).then(c => {
-        resolve(c);
-      });
-    });
-  }
 
-  onSearch(e)
-  {
-    this.refresh();
-  }
+
 
   onFavoriteClick(e) {
     this.favoriteCheck = !this.favoriteCheck;
@@ -112,6 +118,22 @@ export class AXSideMenuComponent {
       this.favoriteItems = this.favoriteItems.filter(c => c.name != item.name);
     }
     this.navMenuService.setFavorites(item, item.data.fav);
+  }
+
+
+  onSearchChanged(text: string) {
+    if (!this.searchChangeObserver) {
+      Observable.create(observer => {
+        this.searchChangeObserver = observer;
+      }).pipe(debounceTime(300))
+        .pipe(distinctUntilChanged())
+        .subscribe(c => {
+          this.searchText = c;
+          this.refresh();
+        });
+    }
+
+    this.searchChangeObserver.next(text);
   }
 
 }
--- src/app/services/nav-menu.service.ts
+++ src/app/services/nav-menu.service.ts
@@ -6,11 +6,12 @@ import { AXNavMenuService } from 'acorex-spa';
 @Injectable()
 export class NavMenuService extends AXNavMenuService {
 
-    private mockItems:MenuItem[]=[
-        { name: "item1", text: "آیتم 1", id: "10", visible:true },
-        { name: "item2", text: "آیتم 2", id: "20", visible:true },
-        { name: "item3", text: "آیتم 3", id: "30", visible:true },
-        { name: "item4", text: "آیتم 4", id: "40", visible:true }
+    private mockItems: MenuItem[] = [
+        { name: "item1", text: "آیتم 1", id: "10", visible: true },
+        { name: "item11", text: "آیتم 11", id: "101", parentId: "10", visible: true },
+        { name: "item2", text: "آیتم 2", id: "20", visible: true },
+        { name: "item3", text: "آیتم 3", id: "30", visible: true },
+        { name: "item4", text: "آیتم 4", id: "40", visible: true },
     ]
 
     getItems(): PromisResult<MenuItem[]> {
@@ -20,7 +21,7 @@ export class NavMenuService extends AXNavMenuService {
     }
     getFavorites(): PromisResult<MenuItem[]> {
         return new PromisResult((resolve) => {
-            resolve(this.mockItems.slice(1,2));
+            resolve(this.mockItems.slice(1, 2));
         });
     }
 
@@ -30,12 +31,16 @@ export class NavMenuService extends AXNavMenuService {
 
     serach(search: string): PromisResult<MenuItem[]> {
         return new PromisResult((resolve) => {
-            resolve(this.mockItems.filter(c=>c.text.includes(search)));
+            debugger;
+            resolve(this.mockItems.filter(c => c.text.includes(search)));
         });
     }
 
     clickItem(item: MenuItem): PromisResult<boolean> {
-        throw new Error("Method not implemented.");
+        return new PromisResult((resolve) => {
+            console.log(item);
+            resolve(true);
+        });
     }
 
 }
