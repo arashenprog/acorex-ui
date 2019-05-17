c62358499dc009c2d6c56f745cbf790279826c45
asafari72 <asafari72@gmail.com>
2019-05-17 15:42:22 +0430

    select box fix style

--- projects/acorex-ui/src/lib/components/form/select-box/select-box.component.ts
+++ projects/acorex-ui/src/lib/components/form/select-box/select-box.component.ts
@@ -10,23 +10,21 @@ import { SelectItem } from "../../../core/select.class";
 import { AXSelectBaseComponent } from "../../../core/base.class";
 import { NgSelectComponent } from "@ng-select/ng-select";
 
+
 @Component({
   selector: "ax-select-box",
   templateUrl: "./select-box.component.html",
   styleUrls: ["./select-box.component.scss"],
   encapsulation: ViewEncapsulation.None
 })
+
 export class AXSelectBoxComponent extends AXSelectBaseComponent {
   @ViewChild(NgSelectComponent) ngSelect: NgSelectComponent;
 
   constructor() {
     super();
   }
-  validate(): Promise<
-    import("../validation/validation.classs").IValidationRuleResult
-  > {
-    throw new Error("Method not implemented.");
-  }
+ 
   @Input() items: any[] = [];
   onClickInner() {
 
