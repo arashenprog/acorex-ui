676d894b6f7930943936a9f5ff6ffd7dd32d4e73
asafari72 <asafari72@gmail.com>
2019-05-20 22:31:08 +0430

    menu item

--- src/app/services/nav-menu.service.ts
+++ src/app/services/nav-menu.service.ts
@@ -14,36 +14,167 @@ export class NavMenuService extends AXNavMenuService {
 
   private mockItems: MenuItem[] = [
     {
-      name: "item1",
+      name: "dashboard",
       text: "Dashboard",
-      id: "10",
+      id: "0",
       visible: true,
       data: { page: DashboardPage, uid: "dashboard" }
     },
     {
-      name: "item2",
-      text: "Http Test",
-      id: "20",
+      name: "customers",
+      text: "Customers",
+      id: "1",
       visible: true,
       data: { page: TestHttpComponent }
     },
     {
-      name: "item2",
-      text: "UI Test",
-      id: "30",
-      visible: true,
-      data: { page: TestPageComponent }
-    },
-    { name: "item3", text: "Item 1", id: "30", visible: true },
-    { name: "item4", text: "Item 2", id: "40", visible: true },
-    {
-      name: "item11",
-      text: "Item 3",
-      id: "101",
-      parentId: "40",
+      name: "suppliers",
+      text: "Suppliers",
+      id: "2",
       visible: true
     },
-    { name: "item12", text: "Item 4", id: "102", parentId: "40", visible: true }
+    {
+      name: "subcontractors",
+      text: "Subcontractors",
+      id: "3",
+      visible: true
+    },
+    
+    {
+      name: "products",
+      text: "Products",
+      id: "4",
+      visible: true
+    },
+    {
+      name: "services",
+      text: "Services",
+      id: "5",
+      visible: true
+    },
+    {
+      name: "leads",
+      text: "Leads",
+      id: "6",
+      visible: true
+    },
+    {
+      name: "quotes",
+      text: "Quotes",
+      id: "7",
+      visible: true
+    },
+    {
+      name: "jobs",
+      text: "Jobs",
+      id: "8",
+      visible: true
+    },
+    {
+      name: "tasks",
+      text: "Tasks",
+      id: "9",
+      visible: true
+    },
+    {
+      name: "orders",
+      text: "Orders",
+      id: "10",
+      visible: true
+    },
+    {
+      name: "invoices",
+      text: "Invoices",
+      id: "11",
+      visible: true
+    },
+    {
+      name: "Calendar",
+      text: "calendar",
+      id: "12",
+      visible: true
+    },
+    { name: "settings", text: "Settings", id: "13", visible: true },
+    {
+      name: "automations",
+      text: "Automations",
+      id: "13_1",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "email_templates",
+      text: "Email Templates",
+      id: "13_2",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "job_templates",
+      text: "Job Templates",
+      id: "13_3",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "sms_templates",
+      text: "SMS Templates",
+      id: "13_4",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "document_templates",
+      text: "Document Templates",
+      id: "13_5",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "preferences",
+      text: "Preferences",
+      id: "13_6",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "users",
+      text: "Users",
+      id: "13_7",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "forms",
+      text: "Forms",
+      id: "13_8",
+      parentId: "13",
+      visible: true
+    },
+    {
+      name: "staff",
+      text: "Staff",
+      id: "14",
+      visible: true
+    },
+    {
+      name: "map",
+      text: "Map",
+      id: "15",
+      visible: true
+    },
+    {
+      name: "report",
+      text: "Report",
+      id: "16",
+      visible: true
+    },
+    {
+      name: "inbox",
+      text: "Inbox",
+      id: "17",
+      visible: true
+    },
   ];
 
   getItems(): PromisResult<MenuItem[]> {
