7290520f05fc21d7cd575d9f9e219e3fad8f1ace
asafari72 <asafari72@gmail.com>
2019-05-24 15:26:36 +0430

    scroll on div

--- projects/acorex-spa/src/lib/layouts/tabular/header/header.layout.ts
+++ projects/acorex-spa/src/lib/layouts/tabular/header/header.layout.ts
@@ -1,68 +1,63 @@
 import { Component, OnInit, DoCheck } from "@angular/core";
-import { BaseMenuItem, AXTabPageService } from 'acorex-ui';
-import { AXHeaderBarMenuService } from '../../shared/api';
-
-
+import { BaseMenuItem, AXTabPageService } from "acorex-ui";
+import { AXHeaderBarMenuService } from "../../shared/api";
 
 @Component({
-    selector: 'ax-header-layout',
-    templateUrl: './header.layout.html',
+  selector: "ax-header-layout",
+  templateUrl: "./header.layout.html"
 })
 export class AXHeaderComponent implements OnInit, DoCheck {
+  fullWidth: boolean = true;
 
-    fullWidth: boolean = true;
+  headerItems: BaseMenuItem[];
 
-    headerItems: BaseMenuItem[];
+  constructor(
+    private headerBarMenuService: AXHeaderBarMenuService,
+    public tabService: AXTabPageService
+  ) {}
 
-    constructor(private headerBarMenuService: AXHeaderBarMenuService, public tabService: AXTabPageService) {
+  ngOnInit(): void {
+    this.headerBarMenuService.getItems().then(c => {
+      this.headerItems = c;
+    });
 
+    let sidebar = document.getElementById("ax-side-menu");
+    let header = document.getElementById("header-content");
+    if (sidebar.classList.contains("d-none")) {
+      header.style.width = window.innerWidth + "px";
+    } else {
+      header.style.width = window.innerWidth - sidebar.offsetWidth + "px";
     }
+  }
+  ngDoCheck(): void {
+    let sidebar = document.getElementById("ax-side-menu");
+    let header = document.getElementById("header-content");
+    if (sidebar.classList.contains("d-none")) {
+      header.style.width = window.innerWidth + "px";
+    } else {
+      header.style.width = window.innerWidth - sidebar.offsetWidth + "px";
+    }
+  }
+  onFullClick() {
+    let side = document.getElementsByTagName("ax-side-menu");
+    let sidebar = document.getElementById("ax-side-menu");
+    let header = document.getElementById("header-content");
+    if (side) {
+      sidebar.classList.toggle("d-none");
+    }
+    if (sidebar.classList.contains("d-none")) {
+      header.style.width = window.innerWidth + "px";
+    } else {
+      header.style.width = window.innerWidth - sidebar.offsetWidth + "px";
+    }
+  }
+  onMouseWheel(e) {
+    console.log("mouse wheel", e);
+    let delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    document.getElementsByClassName("tabs")[0].scrollLeft -= (delta * 40);
+  }
 
-    ngOnInit(): void {
-        this.headerBarMenuService.getItems().then(c => {
-            this.headerItems = c;
-        });
-
-        let sidebar = document.getElementById("ax-side-menu")
-        let header = document.getElementById("header-content")
-        if (sidebar.classList.contains("d-none")) {
-            header.style.width = window.innerWidth + "px";
-        }
-        else {
-            header.style.width = window.innerWidth - sidebar.offsetWidth + "px";
-        }
-    }
-    ngDoCheck(): void {
-        let sidebar = document.getElementById("ax-side-menu")
-        let header = document.getElementById("header-content")
-        if (sidebar.classList.contains("d-none")) {
-            header.style.width = window.innerWidth + "px";
-        }
-        else {
-            header.style.width = window.innerWidth - sidebar.offsetWidth + "px";
-        }
-
-    }
-    onFullClick() {
-        let side = document.getElementsByTagName("ax-side-menu");
-        let sidebar = document.getElementById("ax-side-menu");
-        let header = document.getElementById("header-content");
-        if (side) {
-            sidebar.classList.toggle("d-none")
-        }
-        if (sidebar.classList.contains("d-none")) {
-            header.style.width = window.innerWidth + "px";
-        }
-        else {
-            header.style.width = window.innerWidth - sidebar.offsetWidth + "px";
-        }
-    }
-    onMouseWheel(e) {
-        console.log("onMouseWheel",e)
-    }
-
-    onHeaderClick(e: BaseMenuItem) {
-        this.headerBarMenuService.clickItem(e).then(c => {
-        });
-    }
+  onHeaderClick(e: BaseMenuItem) {
+    this.headerBarMenuService.clickItem(e).then(c => {});
+  }
 }
