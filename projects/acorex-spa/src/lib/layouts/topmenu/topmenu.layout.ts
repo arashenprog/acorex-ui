efbf86ed46d0fc033db3027baaf57ba75e44650f
asafari72 <asafari72@gmail.com>
2019-05-26 00:24:17 +0430

    top menu layout

--- projects/acorex-spa/src/lib/layouts/topmenu/topmenu.layout.ts
+++ projects/acorex-spa/src/lib/layouts/topmenu/topmenu.layout.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BaseMenuItem, AXTabPageService } from "acorex-ui";
+import { AXHeaderBarMenuService } from "../shared/api";
 
 @Component({
-    selector: 'top-menu',
-    templateUrl: './topmenu.layout.html',
-    styleUrls: ['./topmenu.layout.scss']
+  selector: "top-menu",
+  templateUrl: "./topmenu.layout.html",
+  styleUrls: ["./topmenu.layout.scss"]
 })
 export class AXTopMenuLayoutComponent implements OnInit {
-    constructor() { }
+    
+    constructor(private headerBarMenuService: AXHeaderBarMenuService,public tabService: AXTabPageService) {}
+  headerItems: BaseMenuItem[];
 
-    ngOnInit(): void { }
+  ngOnInit(): void {
+    this.headerBarMenuService.getItems().then(c => {
+      this.headerItems = c;
+    });
+  }
+  onHeaderClick(e: BaseMenuItem) {
+    this.headerBarMenuService.clickItem(e).then(c => {});
+  }
+  onMouseWheel(e) {
+    let delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    document.getElementsByClassName("tabs")[0].scrollLeft -= (delta * 40);
+  }
+
 }
