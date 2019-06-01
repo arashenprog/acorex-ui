1231f8182548adddbc82902554f50a37bd091244
a.safari <a.safari@SAFARIA>
2019-06-01 11:38:39 +0430

    doc tab style

--- projects/acorex-ui/src/lib/components/layout/dock-layout/dock-layout.component.ts
+++ projects/acorex-ui/src/lib/components/layout/dock-layout/dock-layout.component.ts
@@ -1,53 +1,54 @@
-import { Component, ElementRef, ViewContainerRef,  ContentChildren, QueryList } from '@angular/core';
+import { Component, ElementRef, ViewContainerRef, ContentChildren, QueryList, ViewEncapsulation } from '@angular/core';
 import * as GoldenLayout from 'golden-layout';
 import { AXDockPanelComponent } from './dock-panel.component';
 declare var $: any;
 
 
 @Component({
-    selector: 'ax-dock-layout',
-    templateUrl: './dock-layout.component.html',
-    styleUrls: ['./dock-layout.component.scss']
+  selector: 'ax-dock-layout',
+  templateUrl: './dock-layout.component.html',
+  styleUrls: ['./dock-layout.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AXDockLayoutComponent  {
+export class AXDockLayoutComponent {
 
 
-    @ContentChildren(AXDockPanelComponent)
-    private panel:QueryList<AXDockPanelComponent>;
-    private config: any;
-    private layout: any;
+  @ContentChildren(AXDockPanelComponent)
+  private panel: QueryList<AXDockPanelComponent>;
+  private config: any;
+  private layout: any;
 
-    constructor(
-        private el: ElementRef,
-        private viewContainer: ViewContainerRef) {
-      
+  constructor(
+    private el: ElementRef,
+    private viewContainer: ViewContainerRef) {
 
-        this.config = {
-            settings:{
-                hasHeaders: true,
-                showPopoutIcon: false,
-                showMaximiseIcon: false,
-                showCloseIcon: false
-            },
-            content: []
-        };
 
-    }
+    this.config = {
+      settings: {
+        hasHeaders: true,
+        showPopoutIcon: false,
+        showMaximiseIcon: false,
+        showCloseIcon: false
+      },
+      content: []
+    };
 
-    ngAfterViewInit(): void {
-        this.panel.forEach(p => {
-            this.config.content.push(p.config())
-        });
+  }
 
-        this.layout = new GoldenLayout(this.config, $('#layoutContainer'));
-        this.layout.registerComponent( 'component', function( container, state ){
-            container.getElement().html( '<h2>' + state.text + '</h2>');
-            //container.getElement().html(state.component);
-            state.render( container.getElement());
-            //console.log(state);
-        });
-        this.layout.init();
-    }
+  ngAfterViewInit(): void {
+    this.panel.forEach(p => {
+      this.config.content.push(p.config());
+    });
+
+    this.layout = new GoldenLayout(this.config, $('#layoutContainer'));
+    this.layout.registerComponent('component', function (container, state) {
+      container.getElement().html('<h2>' + state.text + '</h2>');
+      //container.getElement().html(state.component);
+      state.render(container.getElement());
+      //console.log(state);
+    });
+    this.layout.init();
+  }
 
 
 
