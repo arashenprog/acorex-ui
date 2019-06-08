796859c54ab6160894ebfa18b3417af2ecb5075f
a.safari <a.safari@SAFARIA>
2019-06-08 15:25:18 +0430

    formbuilder dashboard

--- src/app/modules/form-builder/dashboard/dashboard.page.ts
+++ src/app/modules/form-builder/dashboard/dashboard.page.ts
@@ -1,12 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AXBasePageComponent, MenuItem } from 'acorex-ui';
+import { text } from '@angular/core/src/render3';
 
 @Component({
   selector: 'dashboard-page',
   templateUrl: './dashboard.page.html',
-  styleUrls: ['./dashboard.page.scss']
+  styleUrls: ['./dashboard.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class DashboardPage implements OnInit {
-  constructor() { }
-
-  ngOnInit(): void { }
+export class DashboardPage extends AXBasePageComponent {
+  toolbarItemsRight: MenuItem[] = [
+    {
+      icon: "fas fa-pen",
+      style: "btn btn-success",
+      text: "ایجاد"
+    },
+    {
+      icon: "fas fa-sync",
+      style: "btn btn-info",
+      name: "reset",
+      text: "بروزرسانی"
+    }
+  ];
+  cardItems: any[] = [
+    {
+      title: "عنوان شماره یک",
+      image: "assets/images/form.jpg",
+      url: "#1"
+    },
+    {
+      title: "عنوان شماره دو",
+      image: "assets/images/form.jpg",
+      url: "#2"
+    },
+    {
+      title: "عنوان شماره سه",
+      image: "assets/images/form.jpg",
+      url: "#2"
+    }
+  ]
+  onSearch(text) {
+    let filtred = this.cardItems.filter((c)=>{
+      return c.title == text
+    })
+    console.log(filtred,text)
+  }
 }
--- src/app/modules/form-builder/form-builder.module.ts
+++ src/app/modules/form-builder/form-builder.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPage } from './dashboard/dashboard.page';
+import { ACoreXUIModule } from 'acorex-ui';
+import { AcorexSpaModule } from 'acorex-spa';
 
 @NgModule({
   declarations: [DashboardPage],
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    ACoreXUIModule,
+    AcorexSpaModule
+  ],
   exports: [DashboardPage],
   providers: [],
+  entryComponents: [DashboardPage]
 })
-export class FormBuilderModule {}
+export class FormBuilderModule { }
