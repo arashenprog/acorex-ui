b8cc81bbe25d90391e50fb432dc264cacc0dbfe3
arashenprog <arash.oshnoudi@gmail.com>
2019-06-24 15:34:43 +0430

    . sheduler

--- projects/acorex-ui/src/lib/components/calendar/scheduler/scheduler.component.ts
+++ projects/acorex-ui/src/lib/components/calendar/scheduler/scheduler.component.ts
@@ -65,7 +65,6 @@ export class AXSchedulerComponent implements OnInit {
                 this.currentView = c;
             });
             this.toolbarView.items = this.viewItems;
-            debugger;
             this.setView(this.currentView);
         });
     }
