2a7b6489bf02542557e02e2c363bc59d79649b34
arashenprog <arash.oshnoudi@gmail.com>
2019-07-06 18:58:54 +0430

    cakendar

--- projects/acorex-ui/src/lib/components/calendar/calendar-box/calendar-box.component.ts
+++ projects/acorex-ui/src/lib/components/calendar/calendar-box/calendar-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AXDateTime, AXDateTimeRange } from '../../../core/calendar/datetime';
 
 @Component({
     selector: 'ax-calendar-box',
@@ -6,7 +7,54 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./calendar-box.component.scss']
 })
 export class AXCalendarBoxComponent implements OnInit {
-    constructor() { }
+    constructor() {
+        this.value = new AXDateTime()
+    }
 
-    ngOnInit(): void { }
+    view: "year" | "month" | "day" = "day";
+
+    viewRange: AXDateTimeRange;
+
+    private _value: AXDateTime;
+    public get value(): AXDateTime {
+        return this._value;
+    }
+    public set value(v: AXDateTime) {
+        this._value = v;
+        this.viewRange = new AXDateTimeRange(v.startOf("month"), v.endOf("month"))
+    }
+
+
+    ngOnInit(): void {
+
+    }
+
+    prev() {
+        this.navigate(-1)
+    }
+
+    next() {
+        this.navigate(1)
+    }
+
+    navigate(value: number) {
+        let start: AXDateTime;
+        let end: AXDateTime;
+        if (this.view == "day") {
+            start = this.viewRange.startTime.add("month", value).startOf("month");
+            end = this.viewRange.endTime.add("month", value).endOf("month");
+        }
+        else if (this.view == "month") {
+            start = this.viewRange.startTime.add("year", value).startOf("year");
+            end = this.viewRange.endTime.add("year", value).endOf("year");
+        }
+        this.viewRange = new AXDateTimeRange(start, end)
+    }
+
+    changeView() {
+        if (this.view == "day")
+            this.view = "month";
+        else if (this.view == "month")
+            this.view = "year";
+    }
 }
--- projects/acorex-ui/src/lib/components/calendar/calendar-box/calendar-box.module.ts
+++ projects/acorex-ui/src/lib/components/calendar/calendar-box/calendar-box.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AXCalendarBoxComponent } from "./calendar-box.component";
 import { AXPopoverModule } from "../../layout/popover/popover.module";
+import { AXCoreModule } from '../../../core/core.module';
+
+
 
 @NgModule({
   declarations: [AXCalendarBoxComponent],
-  imports: [CommonModule, AXPopoverModule],
+  imports: [CommonModule,AXCoreModule, AXPopoverModule],
   exports: [AXCalendarBoxComponent],
   providers: []
 })
